2d4b847cde185e9dc9868c6ec68cc6ab4bcde498
Anna Christian <anna@noemail.com>
2016-02-29 17:57:00 -0800

    Fix media.py to be PEP8-compliant

--- media.py
+++ media.py
@@ -1,26 +1,27 @@
 class Movie():
-    """ 
-    Class devining a movie.
-    Instance variables: 
-    title, storyline, poster_image_url, trailer_youtube_url
+    """ Class defining a movie.
+
+    Attributes:
+        title (str): movie title
+        storyline (str): brief description of the movie's story line
+        poster_image_url (str): URL of movie's box art
+        trailer_youtube_url (str): URL of movie's trailer on YouTube
+
     Methods:
-    show_trailer()
+        show_trailer()
     """
 
     def __init__(self, title, storyline, poster_img_url, trailer_youtube_url):
         self.title = title
         self.storyline = storyline
-#        self.year = year
-#        self.cast = cast
-#        self.director = director
         self.poster_image_url = poster_img_url
         self.trailer_youtube_url = trailer_youtube_url
 
     def show_trailer(self):
-    	"""
-    	opens trailer_youtube_url in default web browser
-    	"""
-    	
+        """
+        opens trailer_youtube_url in default web browser
+        """
+
         import webbrowser
         webbrowser.open(self.trailer_youtube_url)
 
